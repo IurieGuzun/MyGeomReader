93e32b02d70efde31b7f9b1acf1b58a529e41c80
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-22 10:50:21 -0500

    Initial Commit

---
+++ MyGeomReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyGeomReader
+//
+//  Created by Iurie Guzun on 2021-01-22.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
